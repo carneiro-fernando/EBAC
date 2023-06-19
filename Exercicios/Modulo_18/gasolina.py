bbbc8088959c18d758ccbbc30299e6d9ced4d9dd
carneiro-fernando <carneiro.fernando@icloud.com>
2023-06-19 20:00:19 +0000

    Commit inicial na branch Develop

--- Exercicios/Modulo_18/gasolina.py
+++ Exercicios/Modulo_18/gasolina.py
@@ -3,14 +3,13 @@
 import pandas as pd
 import seaborn as sns
 
-# Criação de dataframe
-gas_df = pd.read_csv("gasolina.csv")
+# Criação do dataset com os dados do arquivo .csv
+data = pd.read_csv('gasolina.csv')
 
-# Visualização
-sns.set_style('whitegrid')
-grafico = sns.lineplot(data=gas_df, x='dia', y='venda')
-grafico.set(title= 'Gráfico - Preço da gasolina no tempo', xlabel='Dia', ylabel='Preço')
+# Criação do gráfico
+grafico = sns.lineplot(data, x="dia", y="venda")
+grafico.set(title="Preço da gasolina 1 a 10 de Julho 2021",xlabel="Dia", ylabel="Preço")
 
-# Armazenamento
+# Armazenamento da figura gerada
 fig = grafico.get_figure()
-fig.savefig('gasolina.png', dpi=300)
+fig.savefig('gasolina.png', dpi=250)
